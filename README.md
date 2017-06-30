e1bef9d5d208746ce37224b2a4b8a3dbdcd0d1bc
Michael Bathke <michael.bathke@sevenval.com>
2017-06-30 15:11:06 +0200

    Added results and docker container

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # visualizer
 
 visualize the gaming data from arena I/O
+
+## install 
+
+```bash
+$ docker build . -t visualizer
+```
+
+## run
+
+```bash
+$ docker run --rm --name visualizer -v /some/content:/usr/share/nginx/html:ro -d nginx
+```
---
+++ dist/script.js
@@ -0,0 +1,96 @@
+(function (namespace) {
+  const ctx = document.querySelector('#canvas').getContext('2d')
+  const DOUBLE_PI = Math.PI * 2
+  ctx.lineWidth = 5
+
+
+  class Grid {
+    constructor() {
+      this.positions = []
+      this.keyFrame = 0
+    }
+
+    draw() {
+      let i = 0, x, y, pos
+
+      ctx.beginPath()
+      // create lines in x direction
+      for (; i < 2; i++) {
+        x = 100 + 100*i
+        ctx.moveTo(x, 0)
+        ctx.lineTo(x, 300)
+      }
+      // create lines in y direction
+      for (i = 0; i < 2; i++) {
+        y = 100 + 100*i
+        ctx.moveTo(0, y)
+        ctx.lineTo(300, y)
+      }
+
+      ctx.strokeStyle = '#000000'
+      ctx.stroke()
+      ctx.closePath()
+
+      pos = this.positions
+      for (i = 0; i < 9; i++) {
+        x = i % 3 | 0
+        y = i / 3 | 0
+        if (pos[i] === 'x') {
+            this.drawX(x, y)
+        } else if (pos[i] === 'o') {
+            this.drawO(x, y)
+        }
+      }
+    }
+
+    drawX(cellX, cellY) {
+      var i = 0, dx, dy;
+      ctx.beginPath();
+      for (i = 0; i < 2; i++) {
+        dx = (cellX * 100) + 10 + (80*i);
+        dy = (cellY * 100) + 10;
+        ctx.moveTo(dx, dy);
+        dx = (cellX * 100) + 90 - (80*i);
+        dy = (cellY * 100) + 90;
+        ctx.lineTo(dx, dy);
+      }
+      ctx.strokeStyle = '#3333ff';
+      ctx.stroke();
+      ctx.closePath();
+    }
+
+    drawO (cellX, cellY) {
+      ctx.beginPath();
+      ctx.arc(cellX*100 + 50,
+              cellY*100 + 50,
+              40, 0, DOUBLE_PI, false);
+      ctx.strokeStyle = '#ff3333';
+      ctx.stroke();
+      ctx.closePath();
+    }
+
+    markCellWithX(x, y) {
+      this.positions[(y * 3) + x] = 'x';
+      this.keyFrame++;
+      this.draw();
+    }
+
+    markCellWithO(x, y) {
+      this.positions[(y * 3) + x] = 'o';
+      this.keyFrame++;
+      this.draw();
+    }
+
+    isMarkedCell(x, y) {
+      return typeof this.positions[(y * 3) + x] !== 'undefined'; 
+    }
+
+    checkDraw() {
+      return false
+    }
+  }
+
+  const gameGrid = new Grid()
+  gameGrid.draw()
+  namespace.Grid = gameGrid;
+}(window.Visualizer || (window.Visualizer = {})))
